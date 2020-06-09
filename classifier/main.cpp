442015d853a821effa96e9238f687c0b0394b0f3
yichaoxiong <yichao.xiong@media-smart.cn>
2020-06-09 17:44:03 +0800

    update for classifer main.cpp

--- classifier/main.cpp
+++ classifier/main.cpp
@@ -42,17 +42,15 @@ samplesCommon::OnnxSampleParams initializeSampleParams(const samplesCommon::Args
     return params;
 }
 
-
 void printHelpInfo()
 {
-    std::cout << "Usage: ./sample_onnx_mnist [-h or --help] [-d or --datadir=<path to data directory>] [--useDLACore=<int>]" << std::endl;
+    std::cout << "Usage: ./build/main [-h or --help] [--imgfp=<path to image>] [--useDLACore=<int>]" << std::endl;
     std::cout << "--help          Display help information" << std::endl;
-    std::cout << "--datadir       Specify path to a data directory, overriding the default. This option can be used multiple times to add multiple directories. If no data directories are given, the default is to use (data/samples/mnist/, data/mnist/)" << std::endl;
+    std::cout << "--imgfp       Specify path to a image file path" << std::endl;
     std::cout << "--useDLACore=N  Specify a DLA engine for layers that support DLA. Value can range from 0 to n-1, where n is the number of DLA engines on the platform." << std::endl;
     std::cout << "--int8          Run in Int8 mode." << std::endl;
     std::cout << "--fp16          Run in FP16 mode." << std::endl;
 }
-
 
 int main(int argc, char** argv)
 {
@@ -81,7 +79,7 @@ int main(int argc, char** argv)
 	{
 		gLogWarning << "---------------------------------------" <<std::endl;
 		timer.start("main-infer");
-		if (!task.infer("data/cat.jpg"))
+		if (!task.infer(args.imgfp))
 		{
 		}
         else
